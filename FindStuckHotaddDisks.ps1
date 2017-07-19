62801e9e9a9acaf0cc6f40225933ba9942017ea2
cac8907 <cac8907@gmail.com>
2017-07-19 15:35:20 -0400

    Refined Working Copy
    Simplified a lot of the code.  Added better error handling

--- FindStuckHotaddDisks.ps1
+++ FindStuckHotaddDisks.ps1
@@ -1,72 +1,50 @@
 ﻿#Written by Adam Congdon (adam.congdon@veeam.com)
+# Requires server 2012 + as some CMDlets are not present in prior versions.
 
 #add the snapin for Veeam
-    Add-PSSnapin VeeamPSSnapin
+    Try
+        {
+            Get-PSSnapin -Registered Veeam* | Add-PSSnapin
+        }
+    Catch 
+        {
+            Write-host "Cannot add Veeam PS Snapin. Is Veeam Console installed locally?" -foregroundcolor red
+            exit
+        }
+    $vbrServer = Read-Host -Prompt "Please Enter Veeam Backup Server you wish to connect or use localhost to connect locally."
+    Try
+        {
+            Disconnect-VBRServer #This is to ensure connections are cleared to prevent failure/duplicate connections to VBRServer
+            Connect-VBRServer -Server $vbrServer -ErrorAction Stop
+        }
+    Catch
+        {
+            Write-Host $Error[0] -ForegroundColor Red
+            exit
+        }
 
 #Menu to pick with PowerCLI version to use
     $title = "Select PowerCLI Version"
-    $message = "Pick which PowerCLI version you have installed `n Mouse over the option for Details"
-    $65 = New-Object System.Management.Automation.Host.ChoiceDescription "&6.5", `
-        "Selects version 6.5 (compatible with vCenter 5.5 through 6.5 excluding 5.5u3)"
-
-    $50 = New-Object System.Management.Automation.Host.ChoiceDescription "&5-6.0", `
-        "Specifies installed version of 6.0 (compatible with vCenter 5.0 through 6.0u2)"
-
-    $40 = New-Object System.Management.Automation.Host.ChoiceDescription "&4.x", `
-        "Specifies vCenter of 4.x which requires untested powerCLI. Please manually adjust script or allow time for updated version"
-
-    $help = New-Object System.Management.Automation.Host.ChoiceDescription "&HELP", `
-        "Prints the URL for the VMware Compatibility Matrix to help you decide"
-
-    $options = [System.Management.Automation.Host.ChoiceDescription[]]($65, $50, $40, $help)
-
-    $result = $host.ui.PromptForChoice($title, $message, $options, 0)
-
-    switch ($result)
+    Try
         {
-            0 
-                {
-                    Try
-                        {
-                            Install-Module -Name VMware.PowerCLI –Scope CurrentUser
-                        }
-                    Catch
-                        {
-                            exit
-                        }
-                }
-            1 
-                {
-                    Try
-                        {
-                            $PowerCLIModulePath = “C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\Modules”
-                            $OldModulePath = [Environment]::GetEnvironmentVariable(‘PSModulePath’,’Machine’)
-                            if ($OldModulePath -notmatch “PowerCLI”) {
-                            Write-Host “[Adding PowerCLI Module directory to Machine PSModulePath]” -ForegroundColor Green
-                            $OldModulePath += “;$PowerCLIModulePath”
-                            [Environment]::SetEnvironmentVariable(‘PSModulePath’,”$OldModulePath”,’Machine’)
-                            } else {
-                            Write-Host “[PowerCLI Module directory already in PSModulePath. No action taken]” -ForegroundColor Cyan
-                            }
-                            Add-PSSnapin vmware.vimautomation.core -ErrorAction Stop
-                        }
-                    Catch
-                        {
-                            Write-Host "Appropriate PowerCLI module not installed or not found in default install path. Please check script for appropriate path"
-                            exit
-                        }
-                }
-            2 
-                {
-                    Write-Host "This PowerCLI/vSphere version combo was not tested. Try using 5x-6.0 option"
-                    exit
-                }
-            3 
-                {
-                    Write-Host "https://www.vmware.com/resources/compatibility/sim/interop_matrix.php#interop&299=&2=" 
-                    exit
-                }
+            Get-PsSnapin -registered -erroraction SilentlyContinue | Add-PsSnapin -ErrorAction SilentlyContinue
         }
+    Catch
+        {
+            $Error
+        }
+    Try
+        {
+            Get-Module -ListAvailable VM* -ErrorAction SilentlyContinue | Import-Module -ErrorAction SilentlyContinue
+        }
+    Catch
+        {
+            write-host $Error
+            exit
+        }
+                            
+        
+         
 
 
 #connect to VC by asking for user's VC
@@ -74,7 +52,7 @@
     connect-viserver -Server $vcenter
 
 #Get Proxy disk list to display in window for viewing.
-    Write-Host "Checking Proxies, please note this time table is relative to total VM count" -ForegroundColor Green
+    Write-Host "`nChecking Proxies, please note this time table is relative to total VM count" -ForegroundColor Green
     $VeeamProxyList = Get-VBRViProxy | Where-Object {$_.ChassisType -eq "ViVirtual"} | Resolve-DnsName -Name {$_.host.name}
     $VMwareProxyList = Get-VM | select Name, {$_.Guest.Hostname}
     
